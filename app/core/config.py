8e3e12da15df07ac6c283486b2c83c1ba8283516
Flavio Faria <135338397+flavifaria@users.noreply.github.com>
2026-03-13 12:30:43 -0300

    Refactor configuration settings for Fast Api
    Updated project name and secret key in settings.

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,18 @@
-#pip install pydantic_settings
-from pydantic_settings import BaseSettings , SettingsConfigDict
+"""
+Arquivo de configuração da aplicação FastAPI.
+"""
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
-    PROJECT_NAME:str="API - Mercearia"
-    VERSION:str="1.0"
-    DATABASE_URL:str="sqlite:///./mercearia.db"
-    SECRET_KEY:str="sua chave secreta"
-    ALGORITH:str="HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES:int = 30
+    PROJECT_NAME: str = "API - Mercearia do João"
+    VERSION: str = "1.0.0"
+    DATABASE_URL: str = "sqlite:///./mercearia.db"
+    SECRET_KEY: str = "sua-chave-super-secreta-aqui-mude-em-producao"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
 
 settings = Settings()
