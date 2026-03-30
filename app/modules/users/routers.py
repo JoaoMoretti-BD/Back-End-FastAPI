7e555daf9659592cb1e828fda20737bfa4e740d0
Flavio Faria <135338397+flavifaria@users.noreply.github.com>
2026-03-30 17:46:05 -0300

    update

--- app/modules/users/routers.py
+++ app/modules/users/routers.py
@@ -4,6 +4,9 @@ from core.database import SessionLocal
 from modules.users.schemas import UserCreate, UserResponse
 from modules.users.services import UserService
 
+from core.dependencies import get_current_user
+from modules.users.models import User
+
 router = APIRouter(prefix="/users", tags=["Utilizadores"])
 
 def get_db():
@@ -18,3 +21,7 @@ def create_user(user_in: UserCreate, db: Session = Depends(get_db)):
     
     service = UserService(db)
     return service.create_user(user_in)
+
+@router.get("/me", response_model=UserResponse)
+def ler_dados_do_meu_perfil(current_user:User = Depends(get_current_user)):
+    return current_user
--- app/modules/users/services.py
+++ app/modules/users/services.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
 from modules.users.repositories import UserRepository
 from modules.users.schemas import UserCreate
+from core.security import get_password_hash 
 
 class UserService:
     def __init__(self, db: Session):
@@ -18,6 +19,6 @@ class UserService:
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Este email já está cadastrado no sistema."
             )
-            
+        user_in.password = get_password_hash(user_in.password)    
         
         return self.repository.create(user_in)
